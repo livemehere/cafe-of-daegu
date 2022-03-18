7162e99a183ef71a3d6c2caa5d8ed91ed69ff2d9
kongtaemin <rhdxoals@gmail.com>
2022-03-19 02:24:51 +0900

    네이버 지도 API 실험흔적

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,13 @@ import data from "../data/data.json";
 function App() {
   return (
     <div className="App">
-      {data.map((i) => {
-        return (
-          <div key={i["연번"]}>
-            <h4>{i["소재지(도로명)"]}</h4>
-          </div>
-        );
-      })}
+      {/*{data.map((i) => {*/}
+      {/*  return (*/}
+      {/*    <div key={i["연번"]}>*/}
+      {/*      <h4>{i["소재지(도로명)"]}</h4>*/}
+      {/*    </div>*/}
+      {/*  );*/}
+      {/*})}*/}
     </div>
   );
 }
